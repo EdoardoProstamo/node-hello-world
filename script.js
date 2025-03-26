d0db29681ece5d9cacdccaa460381289374e128b
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-03-26 01:55:15 +0100

    feat: messaggio stampato nel terminale

--- script.js
+++ script.js
@@ -4,3 +4,5 @@
 // 4. Impostiamo un nuovo script “watch” in package.json che possa essere lanciato con npm run watch e che aggiorni in tempo reale le modifiche ai nostri file. Lanciamolo e proviamo a cambiare il nostro codice in modo che stampi nel terminale “Hello Boolean”. Dovremmo vedere il terminale senza fermare e rilanciare il server.
 // Bonus
 // Proviamo a modificare la nostra app Node, in modo da passare dal terminale una frase diversa da “Hello Boolean”. Forse potrà servirvi qualcosa vista ieri!
+
+console.log('Hello World');
